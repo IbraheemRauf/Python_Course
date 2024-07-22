8ba652dec2acf7836f9cf7f713e7ac8dc109ab33
IbraheemRauf <ibraheemrauf678@gmail.com>
2024-07-22 17:52:13 +0500

    add: raisingerror

--- day30onward/raisingerror.py
+++ day30onward/raisingerror.py
@@ -1,3 +1,26 @@
-a = int (input ("enter any value between 5 and 9"))
-if a < 5 or a >9:
-    raise ValueError("value should be between 5 and 9")
+
+questions = {
+    "What is the capital of India": "Delhi",
+    "What is the National Bird of India": "Peacock",
+    "What is the National Animal of India": "Tiger",
+    "How many states are there in India": "28",
+}
+Options = [
+    ["Delhi", "Kolkata", "Mumbai", "Chennai"],
+    ["Pigeon", "Crow", "humming bird", "Peacock"],
+    ["Elephant", "Lion", "Tiger", "Liger"],
+    ["9", "28", "31", "23"],
+]
+# print(len(questions))
+l = len(questions)
+i = 0
+for key, value in questions.items():
+    # print(key)
+    # print(value)
+    ans = input(f"{key}?{Options[i]}: ")
+    i = i + 1
+    if value == ans:
+        continue
+    else:
+        print("You are out")
+        break
